dc12a4c8164d16f9b92dec0f5516751ccec55cd2
a-keofteian <a.keofteian@zeroandone.me>
2021-02-08 16:51:49 +0200

    Env switch setup update

--- app/config/awsConfig.ts
+++ app/config/awsConfig.ts
@@ -1,33 +1,37 @@
+import { 
+  IDENTITY_POOL_ID, 
+  USER_POOL_ID, 
+  USER_POOL_WEB_CLIENT_ID,
+  NAME,
+  ENDPOINT,
+  APP_ID,
+  BUCKET,
+  REGION
+} from '../env.json';
+
 const awsConfig = {
   Auth: {
-    // REQUIRED only for Federated Authentication - Amazon Cognito Identity Pool ID
-    identityPoolId: 'eu-west-1:d941f346-161a-476b-9964-934dcd8b1880',
-    // REQUIRED - Amazon Cognito Region
-    region: 'eu-west-1',
-    // // OPTIONAL - Amazon Cognito Federated Identity Pool Region
-    // // Required only if it's different from Amazon Cognito Region
-    // identityPoolRegion: 'eu-west-1',
-    // OPTIONAL - Amazon Cognito User Pool ID
-    userPoolId: 'eu-west-1_3TPQjLLLD',
-    // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
-    userPoolWebClientId: '3m2jo5h6g5mj3ft40vnhi3ar5a',
+    identityPoolId: IDENTITY_POOL_ID,
+    region: REGION,
+    userPoolId: USER_POOL_ID,
+    userPoolWebClientId: USER_POOL_WEB_CLIENT_ID,
   },
   API: {
     endpoints: [
       {
-        name: 'Market',
-        endpoint: 'https://c3lngazysb.execute-api.eu-west-1.amazonaws.com/dev',
-        region: 'eu-west-1',
+        name: NAME,
+        endpoint: ENDPOINT,
+        region: REGION,
       },
     ],
   },
   PushNotification: {
-    appId: '1:902793611820:android:d3f9ff5bbba04d6c08df89',
+    appId: APP_ID,
   },
   Storage: {
     AWSS3: {
-      bucket: 'relife-market-storage-dev', //REQUIRED -  Amazon S3 bucket name
-      region: 'eu-west-1', //OPTIONAL -  Amazon service region
+      bucket: BUCKET,
+      region: REGION,
     },
   },
 };
